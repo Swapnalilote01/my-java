4945e727f52c1eeb012640d6ff4fcc041c6ca702
Swapnalilote01 <112958740+Swapnalilote01@users.noreply.github.com>
2022-09-13 14:19:21 +0530

    Add files via upload

---
+++ Overload1.java
@@ -0,0 +1,23 @@
+class Overload1
+{
+static void address(int p)
+{
+System.out.println("pincode:" +p);
+}
+static void address(int p,String ps)
+{
+System.out.println("pincode:"+p+"police station:"+ps);
+}
+static void address(int p,String ps,String as)
+{
+System.out.println("pincode:"+p+"police station:"+ps+"assembly:"+as);
+}
+public static void main(String args[])
+{
+address(421505);
+address(421505,"vithalwadi");
+address(421505,"vithalwadi","assembly");
+}
+}
+
+ 
