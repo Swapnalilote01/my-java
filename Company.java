bd5c2f99a579caf5bafeb505deef21e6941564dc
Swapnalilote01 <112958740+Swapnalilote01@users.noreply.github.com>
2022-09-15 15:00:41 +0530

    Add files via upload

---
+++ Company.java
@@ -0,0 +1,18 @@
+class Company
+{
+public static void main(String[]args)
+{
+int a=10000;
+int i=30000;
+int e1=11000;
+int e2=5000;
+int e3=i;
+e3=i-e1-e2;
+
+System.out.println("average salary of each employees:" + "="+ 10000);
+System.out.println("total salary of employees:" + "="+ i);
+System.out.println("salary of first employee:" + " ="+ 11000);
+System.out.println("salary of second employee:" + " ="+ 5000);
+System.out.println("salary of third employee:" + " ="+ e3);
+}
+}
