4a7e88059688e798108c09be96da08a5b8109cc9
Swapnalilote01 <112958740+Swapnalilote01@users.noreply.github.com>
2022-09-22 15:47:24 +0530

    Add files via upload

---
+++ average/Population.java
@@ -0,0 +1,16 @@
+package average;
+
+public class Population {
+	public static void main(String[]args) {
+		int a=175000;
+		int b=262500;
+		int c=87500;
+		int d=50;
+		int e=d/10;
+		System.out.println("Increased population in 10 years = "  +c);
+		System.out.println("Increased population in 10 years = "  +d+ "%");
+		System.out.println("Increased population in 1 years = "  +e+ "%");
+	
+	}
+
+}
